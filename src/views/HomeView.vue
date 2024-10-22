94c856475ebf6ef43abe1c7a944adb77c1b2fb1a
KIMMINJAE-4value <yt90as@4valuesoft.com>
2024-10-22 10:31:50 +0900

    네이버 맵 불러오기(컴포넌트)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,33 @@
 <script lang="ts">
 import { ref } from 'vue'
+import { NaverMap } from '@naver-maps/vue'
 
 export default {
+  components: {
+    NaverMap,
+  },
   setup() {
     const hello = ref()
-    return { hello }
+    const onLoadMap = (map: naver.maps.Map) => {
+      console.log(map)
+      // 지도 로드 완료 시 실행되는 함수
+    }
+
+    return { hello, onLoadMap }
   },
 }
 </script>
 
 <template>
-  <div>vue</div>
+  <NaverMap
+    style="width: 100vw; height: 100vh"
+    clientId="y7hgcvgnrx"
+    @onLoaded="onLoadMap"
+    :mapOptions="{
+      latitude: 37.51347,
+      longitude: 127.041722,
+      zoom: 14,
+    }"
+  >
+  </NaverMap>
 </template>
